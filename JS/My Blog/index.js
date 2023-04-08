64238ab5fc97d5eb4e8c4c783eb0eba963ba4214
Summer <nhamhuehue@gmail.com>
2023-04-08 15:47:28 +0700

    Add my blog

---
+++ JS/My Blog/index.js
@@ -0,0 +1,22 @@
+
+    // // Lấy danh sách tất cả các menu item và sub-menu item
+    // var menuItems = document.querySelectorAll('.menu-item');
+    // var subMenuItems = document.querySelectorAll('.sub-menu a');
+    
+    // // Thêm sự kiện hover cho từng menu item và sub-menu item
+    // menuItems.forEach(function(menuItem) {
+    //     menuItem.addEventListener('mouseenter', function() {
+    //         this.querySelector('.sub-menu').style.display = 'block';
+    //     });
+    //     menuItem.addEventListener('mouseleave', function() {
+    //         this.querySelector('.sub-menu').style.display = 'none';
+    //     });
+    // });
+    // subMenuItems.forEach(function(subMenuItem) {
+    //     subMenuItem.addEventListener('mouseenter', function() {
+    //         this.parentNode.parentNode.style.display = 'block';
+    //     });
+    //     subMenuItem.addEventListener('mouseleave', function() {
+    //         this.parentNode.parentNode.style.display = 'none';
+    //     });
+    // });
